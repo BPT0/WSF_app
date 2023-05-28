6d1f60816b86d5c7c2ded943a74810281599680d
rimeir <128125865+rimeir@users.noreply.github.com>
2023-05-29 00:08:37 +0900

    MovieDto.java 주석 추가

--- src/com/demo/MovieDto.java
+++ src/com/demo/MovieDto.java
@@ -1,14 +1,15 @@
 package com.demo;
 
-
-public class MovieDto {
+//로직을 갖고 있지 않는 순수한 데이터 객체
+//속성과 그 속성에 접근하기 위한 getter, setter 메소드만 가진 클레스
+public class MovieDto {  //Movie 객체
     private int idx;
     private String title;
     private String image;
     private String content;
 
 
-    public MovieDto(int idx, String title, String image, String content) {
+    public MovieDto(int idx, String title, String image, String content) { //영화 데이터(idx, 제목, 이미지 이름, 내용)
         this.idx = idx;
         this.title = title;
         this.image = image;
